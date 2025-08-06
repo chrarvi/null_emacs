3edb379293363f44b07f4aa3fb38b2003fadc38c
Christoffer Arvidsson <christoffer@arvidson.nu>
2025-08-06 21:54:19 +0200

    Fix auctex

--- modules/null-org-latex.el
+++ modules/null-org-latex.el
@@ -153,71 +153,69 @@
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
 
-(use-package org-latex-preview
-  :ensure nil
-  :hook (org-mode . org-latex-preview)
-  :config
-  ;; Increase preview width
-  (plist-put org-latex-preview-appearance-options
-             :page-width 1.0)
+;; (use-package org-latex-preview
+;;   :hook (org-mode . org-latex-preview)
+;;   :config
+;;   ;; Increase preview width
+;;   (plist-put org-latex-preview-appearance-options
+;;              :page-width 1.0)
 
-  ;No blur when scaling
-  (setq org-latex-preview-process-default 'dvisvgm)
+;;   ;No blur when scaling
+;;   (setq org-latex-preview-process-default 'dvisvgm)
 
-  ;; Turn on auto-mode, it's built into Org and much faster/more featured than
-  ;; org-fragtog. (Remember to turn off/uninstall org-fragtog.)
-  (add-hook 'org-mode-hook 'org-latex-preview-auto-mode)
-  ;; Block C-n and C-p from opening up previews when using auto-mode
-  (add-hook 'org-latex-preview-auto-ignored-commands 'next-line)
-  (add-hook 'org-latex-preview-auto-ignored-commands 'previous-line)
+;;   ;; Turn on auto-mode, it's built into Org and much faster/more featured than
+;;   ;; org-fragtog. (Remember to turn off/uninstall org-fragtog.)
+;;   (add-hook 'org-mode-hook 'org-latex-preview-auto-mode)
+;;   ;; Block C-n and C-p from opening up previews when using auto-mode
+;;   (add-hook 'org-latex-preview-auto-ignored-commands 'next-line)
+;;   (add-hook 'org-latex-preview-auto-ignored-commands 'previous-line)
 
-  ;; Enable consistent equation numbering
-  (setq org-latex-preview-numbered t)
+;;   ;; Enable consistent equation numbering
+;;   (setq org-latex-preview-numbered t)
 
-  ;; Bonus: Turn on live previews.  This shows you a live preview of a LaTeX
-  ;; fragment and updates the preview in real-time as you edit it.
-  ;; To preview only environments, set it to '(block edit-special) instead
-  (setq org-latex-preview-live t)
+;;   ;; Bonus: Turn on live previews.  This shows you a live preview of a LaTeX
+;;   ;; fragment and updates the preview in real-time as you edit it.
+;;   ;; To preview only environments, set it to '(block edit-special) instead
+;;   (setq org-latex-preview-live t)
 
-  ;; More immediate live-previews -- the default delay is 1 second
-  (setq org-latex-preview-live-debounce 0.25)
+;;   ;; More immediate live-previews -- the default delay is 1 second
+;;   (setq org-latex-preview-live-debounce 0.25)
 
-  (defun my/org-latex-preview-uncenter (ov)
-    (overlay-put ov 'before-string nil))
-  (defun my/org-latex-preview-recenter (ov)
-    (overlay-put ov 'before-string (overlay-get ov 'justify)))
-  (defun my/org-latex-preview-center (ov)
-    (save-excursion
-      (goto-char (overlay-start ov))
-      (when-let* ((elem (org-element-context))
-                  ((or (eq (org-element-type elem) 'latex-environment)
-                       (string-match-p "^\\\\\\[" (org-element-property :value elem))))
-                  (img (overlay-get ov 'display))
-                  (prop `(space :align-to (- center (0.55 . ,img))))
-                  (justify (propertize " " 'display prop 'face 'default)))
-        (overlay-put ov 'justify justify)
-        (overlay-put ov 'before-string (overlay-get ov 'justify)))))
-  (define-minor-mode org-latex-preview-center-mode
-    "Center equations previewed with `org-latex-preview'."
-    :global nil
-    (if org-latex-preview-center-mode
-        (progn
-          (add-hook 'org-latex-preview-overlay-open-functions
-                    #'my/org-latex-preview-uncenter nil :local)
-          (add-hook 'org-latex-preview-overlay-close-functions
-                    #'my/org-latex-preview-recenter nil :local)
-          (add-hook 'org-latex-preview-overlay-update-functions
-                    #'my/org-latex-preview-center nil :local))
-      (remove-hook 'org-latex-preview-overlay-close-functions
-                    #'my/org-latex-preview-recenter)
-      (remove-hook 'org-latex-preview-overlay-update-functions
-                    #'my/org-latex-preview-center)
-      (remove-hook 'org-latex-preview-overlay-open-functions
-                    #'my/org-latex-preview-uncenter))))
+;;   (defun my/org-latex-preview-uncenter (ov)
+;;     (overlay-put ov 'before-string nil))
+;;   (defun my/org-latex-preview-recenter (ov)
+;;     (overlay-put ov 'before-string (overlay-get ov 'justify)))
+;;   (defun my/org-latex-preview-center (ov)
+;;     (save-excursion
+;;       (goto-char (overlay-start ov))
+;;       (when-let* ((elem (org-element-context))
+;;                   ((or (eq (org-element-type elem) 'latex-environment)
+;;                        (string-match-p "^\\\\\\[" (org-element-property :value elem))))
+;;                   (img (overlay-get ov 'display))
+;;                   (prop `(space :align-to (- center (0.55 . ,img))))
+;;                   (justify (propertize " " 'display prop 'face 'default)))
+;;         (overlay-put ov 'justify justify)
+;;         (overlay-put ov 'before-string (overlay-get ov 'justify)))))
+;;   (define-minor-mode org-latex-preview-center-mode
+;;     "Center equations previewed with `org-latex-preview'."
+;;     :global nil
+;;     (if org-latex-preview-center-mode
+;;         (progn
+;;           (add-hook 'org-latex-preview-overlay-open-functions
+;;                     #'my/org-latex-preview-uncenter nil :local)
+;;           (add-hook 'org-latex-preview-overlay-close-functions
+;;                     #'my/org-latex-preview-recenter nil :local)
+;;           (add-hook 'org-latex-preview-overlay-update-functions
+;;                     #'my/org-latex-preview-center nil :local))
+;;       (remove-hook 'org-latex-preview-overlay-close-functions
+;;                     #'my/org-latex-preview-recenter)
+;;       (remove-hook 'org-latex-preview-overlay-update-functions
+;;                     #'my/org-latex-preview-center)
+;;       (remove-hook 'org-latex-preview-overlay-open-functions
+;;                     #'my/org-latex-preview-uncenter))))
 
 ;; Beamer support
-(use-package ox-beamer
-  :ensure nil)
+(use-package ox-beamer-lecture)
 
 ;; Latex
 ;; (use-package org-fragtog
